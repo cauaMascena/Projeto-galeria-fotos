60ed234942ee6b02d3ca4aa51f4c8766ea663def
cauaMascena <cauasinho656@gmail.com>
2021-10-05 10:22:22 -0300

    commit 05/10/2021

--- galeria.js
+++ galeria.js
@@ -1,78 +1,71 @@
 "use strict"
-
-const imagens = [
-    "./img/aperolSpritz.jpg",
-    "./img/caipirinhaMorango.jpg",
-    "./img/margarita.jpg",
-    "./img/sexOnTheBeach.jpg",
-    "./img/spritz.jpg",
-    "./img/tonica.jpg",
-    "./img/tropical.jpg",
-    "./img/whisky.jpg"
-]
-
-const limparId = (urlImagem) => urlImagem.split("/")[2]
-    .split(".")[0]
-    .replace("", "-")
-    .toLowerCase()
-
+ 
+const limpar = (elemento) => {
+    while (elemento.firstChild) {
+        elemento.removeChild(elemento.lastChild)
+    }
+} 
+ 
+const pegarImagens = (raca) => fetch(`https://dog.ceo/api/breed/${raca}/images`);
+ 
+const procurarImagens = async (evento) => {
+ 
+    if(evento.key === 'Enter') {
+        const raca = evento.target.value;
+        const imagensResponse = await pegarImagens(raca);
+        const imagens = await imagensResponse.json();
+    
+        limpar(document.querySelector(".galeria-container"));
+        limpar(document.querySelector(".slide-container"));
+        carregarImagens(imagens.message);
+        carregarSlides(imagens.message);
+    }
+ 
+}
+ 
+// barra separa os elementos
+const limparId = (urlImagem) => {
+    const posicaoBarra = urlImagem.lastIndexOf('/') + 1;
+    const posicaoPonto = urlImagem.lastIndexOf('.');
+    return urlImagem.substring(posicaoBarra, posicaoPonto)
+}
+ 
 const criarItem = (urlImagem) => {
-    const container = document.querySelector(".galeria-container")
-
-    const novoLink = document.createElement("a")
-    novoLink.href = urlImagem
-    novoLink.href = `#${limparId(urlImagem)}`
-    novoLink.classList.add("galeria-itens")
-    novoLink.innerHTML = `<img src="${urlImagem}" alt="">`
-    container.appendChild(novoLink)
-
+    const container = document.querySelector(".galeria-container");
+    const novoLink = document.createElement("a");
+    novoLink.href = `#${limparId(urlImagem)}`;
+    novoLink.classList.add("galeria-itens");
+    novoLink.innerHTML = `<img src="${urlImagem}">`;
+    container.appendChild(novoLink);
+ 
 }
-
-const carregarImagens = () => imagens.forEach(criarItem)
-
-carregarImagens()
-
-
-
+ 
+const carregarImagens = (imagens) => imagens.forEach(criarItem);
+ 
 const criarSlide = (urlImagem, indice, arr) => {
-    const slideContainer = document.querySelector(".slide-container")
-
-    const novoSlide = document.createElement("div")
-    novoSlide.href = urlImagem
-    novoSlide.classList.add("slide")
-    novoSlide.id = limparId(urlImagem)
-
-    const indice = indice > 0 ? indice -1 : arr.length -1
-    const idAnterior = limparId(arr[indiceAnterior])
-
-    const indiceProximo = indice < arr.length -1 ? indice +1 :0
-    const idProximo = limparId(arr[indiceProximo])
-
-    // const indiceAnterior = indice - 1
-    // if (indice >0) {
-    //     indiceAnterior = indice - 1
-    // }else{
-    //     indiceAnterior = arr.length - 1
-    // }
-   
-
-    novoSlide.innerHTML = ` <div class"imagem-container">
-    <a href="#" class="fechar">&#10006;</a>
-    <a href="" class="navegacao anterior">&#171;</a>
-    <img src="${urlImagem}" alt="">
-    <a href="" class="navegacao proximo">&#187;</a>
-    </div>`
-
-    slideContainer.appendChild(novoSlide)
-
+    const container = document.querySelector('.slide-container');
+    const slide = document.createElement('div');
+    slide.classList.add('slide');
+    slide.id = limparId(urlImagem);
+ 
+    const indiceAnterior = indice > 0 ? indice - 1 : arr.length - 1;    
+    const idAnterior = limparId(arr[indiceAnterior]);
+ 
+    const indiceProximo = indice < arr.length - 1 ? indice + 1 : 0;
+    const idProximo = limparId(arr[indiceProximo]);
+ 
+    slide.innerHTML = `
+        <div class="imagem-container">
+            <a href="#" class="fechar">&#10006;</a>
+            <a href="#${idAnterior}" class="navegacao anterior">&#171;</a>
+            <img src="${urlImagem}" alt="">
+            <a href="#${idProximo}" class="navegacao proximo">&#187;</a>
+        </div>
+    `
+    container.appendChild(slide);
 }
-
-
-const carregarSlides = () => imagens.forEach(criarItem)
-
-carregarSlides()
-
-
-
-
-
+ 
+const carregarSlides = (imagens) => imagens.forEach(criarSlide);
+ 
+document.querySelector(".pesquisa-container input")
+        .addEventListener('keypress', procurarImagens);
